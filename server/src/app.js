a3f1f248fd543e030e9f9e31ac14bbda737dd908
Brian Fajardo <bmf5@hotmail.com>
2017-10-05 20:40:56 -0400

    update build

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const compress = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
-// const path = require('path');
+const path = require('path');
 
 const feathers = require('feathers');
 const configuration = require('feathers-configuration');
@@ -24,7 +24,8 @@ app
   .use(compress())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
-  // .use(feathers.static(path.resolve(__dirname, '../../build'))) production
+  .use(feathers.static('build'))
+  .use('*', res => res.sendFile(path.resolve('build/index.html')))
   .configure(mongoDb)
   .configure(hooks())
   .configure(rest())
