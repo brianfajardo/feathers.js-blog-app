6280990c7110b839939075a58319bcef2db38ea9
Brian Fajardo <bmf5@hotmail.com>
2017-10-14 02:33:14 -0400

    Remove unused module

--- src/actions/feathersClient.js
+++ src/actions/feathersClient.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import feathers from 'feathers-client'
 
-// Feathers setup for rest and auth
+// Feathers setup for RESTful API and auth.
 export const client = feathers()
-  .configure(feathers.hooks())
   .configure(feathers.rest(process.env.REACT_APP_REST_CLIENT_URL).axios(axios))
   .configure(feathers.authentication({ storage: window.localStorage }))
 
+// Referencing the server-side service.
 export const userService = client.service('/user')
 export const postService = client.service('/post')
